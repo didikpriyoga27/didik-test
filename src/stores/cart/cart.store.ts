4ac90a43f095a023956851d6bf42803ac13a66d2
didikpriyoga <didikpriyoga27@gmail.com>
2024-10-30 13:09:37 +0700

    fix: cart store

--- src/stores/cart/cart.store.ts
+++ src/stores/cart/cart.store.ts
@@ -2,109 +2,104 @@
 
 import { Product } from "@/app/products/type";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 import { CartItem, CartStore } from "./type";
 
-export const useCartStore = create<CartStore>()((set) => ({
-  products: JSON.parse(<string>localStorage.getItem("cart")) ?? [],
+export const useCartStore = create<CartStore>()(
+  persist(
+    (set) => ({
+      products: [] as CartItem[],
 
-  /**
-   * Adds an item to the cart. If the item already exists in the cart,
-   * it increases the quantity of the item. Otherwise, it adds the item
-   * to the cart. The updated cart is then stored in local storage.
-   *
-   * @param {CartItem} p - The item to add to the cart.
-   */
-  addItem: (p: CartItem) => {
-    set((state) => {
-      if (state.products.map((p) => p.product.id).includes(p.product.id)) {
-        state.increaseQuantity(p.product.id);
-      } else {
-        state.products.push(p);
-      }
-
-      localStorage.setItem("cart", JSON.stringify(state.products));
-
-      return { products: state.products };
-    });
-  },
-
-  /**
-   * Removes an item from the cart based on its product ID.
-   * The updated cart is then stored in local storage.
-   *
-   * @param {Product["id"]} id - The ID of the product to remove from the cart.
-   */
-  removeItem: (id: Product["id"]) => {
-    set((state) => {
-      const updatedCart = state.products.filter((p) => p.product.id !== id);
-
-      localStorage.setItem("cart", JSON.stringify(updatedCart));
-
-      return { products: updatedCart };
-    });
-  },
-
-  /**
-   * Removes all items from the cart.
-   * The updated cart is then stored in local storage.
-   */
-  removeAll: () =>
-    set(() => {
-      localStorage.setItem("cart", "[]");
-
-      return { products: [] };
-    }),
-
-  /**
-   * Increases the quantity of a product in the cart by one.
-   * If the product does not exist in the cart, it does nothing.
-   * The updated cart is then stored in local storage.
-   *
-   * @param {Product["id"]} id - The ID of the product to increase the quantity of.
-   */
-  increaseQuantity: (id: Product["id"]) => {
-    set((state) => {
-      const updatedCart = state.products.map((p) => {
-        if (p.product.id === id) {
-          p.qty++;
-
-          return p;
-        }
-
-        return p;
-      });
-
-      localStorage.setItem("cart", JSON.stringify(updatedCart));
-
-      return { products: updatedCart };
-    });
-  },
-
-  /**
-   * Decreases the quantity of a product in the cart by one.
-   * If the product does not exist in the cart or the quantity is zero,
-   * the product is removed from the cart.
-   * The updated cart is then stored in local storage.
-   *
-   * @param {Product["id"]} id - The ID of the product to decrease the quantity of.
-   */
-  decreaseQuantity: (id: Product["id"]) => {
-    set((state) => {
-      const updatedCart = state.products.filter((p) => {
-        if (p.product.id === id && p.qty > 0) {
-          p.qty--;
-
-          if (p.qty <= 0) {
-            return false;
+      /**
+       * Adds an item to the cart. If the item already exists in the cart,
+       * it increases the quantity of the item. Otherwise, it adds the item
+       * to the cart.
+       *
+       * @param {CartItem} p - The item to add to the cart.
+       */
+      addItem: (p: CartItem) => {
+        set((state) => {
+          if (state.products.map((p) => p.product.id).includes(p.product.id)) {
+            state.increaseQuantity(p.product.id);
+          } else {
+            state.products.push(p);
           }
-        }
 
-        return true;
-      });
+          return { products: state.products };
+        });
+      },
 
-      localStorage.setItem("cart", JSON.stringify(updatedCart));
+      /**
+       * Removes an item from the cart based on its product ID.
+       *
+       * @param {Product["id"]} id - The ID of the product to remove from the cart.
+       */
+      removeItem: (id: Product["id"]) => {
+        set((state) => {
+          const updatedCart = state.products.filter((p) => p.product.id !== id);
 
-      return { products: updatedCart };
-    });
-  },
-}));
+          return { products: updatedCart };
+        });
+      },
+
+      /**
+       * Removes all items from the cart.
+       */
+      removeAll: () =>
+        set(() => {
+          return { products: [] };
+        }),
+
+      /**
+       * Increases the quantity of a product in the cart by one.
+       * If the product does not exist in the cart, it does nothing.
+       *
+       * @param {Product["id"]} id - The ID of the product to increase the quantity of.
+       */
+      increaseQuantity: (id: Product["id"]) => {
+        set((state) => {
+          const updatedCart = state.products.map((p) => {
+            if (p.product.id === id) {
+              p.qty++;
+
+              return p;
+            }
+
+            return p;
+          });
+
+          return { products: updatedCart };
+        });
+      },
+
+      /**
+       * Decreases the quantity of a product in the cart by one.
+       * If the product does not exist in the cart or the quantity is zero,
+       * the product is removed from the cart.
+       *
+       * @param {Product["id"]} id - The ID of the product to decrease the quantity of.
+       */
+      decreaseQuantity: (id: Product["id"]) => {
+        set((state) => {
+          const updatedCart = state.products.filter((p) => {
+            if (p.product.id === id && p.qty > 0) {
+              p.qty--;
+
+              if (p.qty <= 0) {
+                return false;
+              }
+            }
+
+            return true;
+          });
+
+          return { products: updatedCart };
+        });
+      },
+    }),
+    {
+      name: "cart",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
